f158e78772010ddfd461b392ee77253160d1be9f
Viacheslav Yemets <viacheslav.emets@gmail.com>
2025-02-19 19:51:54 +0200

    vercel config and static files for swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,20 @@ import { ConfigModule } from '@nestjs/config';
 import { TestingModule } from './features/testing/testing.module';
 import { CoreModule } from './core/core.module';
 import { BloggersPlatformModule } from './features/bloggers-platform/bloggers-platform.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GLOBAL_PREFIX } from './setup/global-prefix.setup';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL || '', {
       dbName: process.env.DB_NAME || 'incubator',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot:
+        process.env.NODE_ENV === 'development' ? '/' : `/${GLOBAL_PREFIX}`,
     }),
     TestingModule,
     CoreModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
+import { createWriteStream } from 'fs';
+import { get } from 'http';
+
+const serverUrl = 'http://localhost:3000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appSetup(app);
   app.enableCors();
   await app.listen(3000);
+
+  // get the swagger json file (if app is running in development mode)
+  if (process.env.NODE_ENV === 'development') {
+    // write swagger ui files
+    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+    });
+
+    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    });
+
+    get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+      },
+    );
+    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    });
+  }
 }
 bootstrap();
