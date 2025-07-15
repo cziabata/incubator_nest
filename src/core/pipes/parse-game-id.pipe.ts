ce973724bb909f99698ab6ec56243a5dc327bfb7
Viacheslav Yemets <viacheslav.emets@gmail.com>
2025-07-15 20:09:17 +0300

    Add NotFoundException in ParseGameIdPipe for improved error handling when a game ID is a valid UUID, ensuring proper validation of game IDs.

--- src/core/pipes/parse-game-id.pipe.ts
+++ src/core/pipes/parse-game-id.pipe.ts
@@ -1,8 +1,14 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException, NotFoundException } from '@nestjs/common';
+import { isUUID } from 'class-validator';
 
 @Injectable()
 export class ParseGameIdPipe implements PipeTransform<string, number> {
   transform(value: string, metadata: ArgumentMetadata): number {
+
+    if (isUUID(value)) {
+      throw new NotFoundException('Game not found');
+    }
+
     const parsedValue = parseInt(value, 10);
     
     if (isNaN(parsedValue) || !Number.isInteger(parsedValue) || parsedValue <= 0) {
